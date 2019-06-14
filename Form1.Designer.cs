503cc91722464285fe6035d7a491ba8631dd9fd6
Mauricio Gaggero <51423735+maurigaggero@users.noreply.github.com>
2019-06-14 15:11:05 -0300

    temperatura máxima del box ahora 85° (antes 90°)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -283,7 +283,7 @@
             0});
             this.numericUpDown2.Location = new System.Drawing.Point(216, 46);
             this.numericUpDown2.Maximum = new decimal(new int[] {
-            90,
+            85,
             0,
             0,
             0});
